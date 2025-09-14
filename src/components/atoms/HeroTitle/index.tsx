e105ccf750483707564dec35d4755b5cd520d9c7
ferney <ferney.montoyag@gmail.com>
2025-09-14 10:13:54 -0500

    Change hero title from 'Frontend' to 'Sotfware'

--- src/components/atoms/HeroTitle/index.tsx
+++ src/components/atoms/HeroTitle/index.tsx
@@ -2,7 +2,7 @@ const HeroTitle = () => {
   return (
     <h1 className="font-black text-3xl md:text-xxxl md:leading-none mb-4">
       I&apos;m Ferney Montoya <br />
-      <span className="text-primaryColor">Front-end</span> Developer
+      <span className="text-primaryColor">Software</span> Developer
     </h1>
   );
 };
